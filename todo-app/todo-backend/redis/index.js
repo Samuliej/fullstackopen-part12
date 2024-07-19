d66e9e04b0a98343e3a56a16190b8756868afb70
Samuli Toppi <samuli.toppi@outlook.com>
2024-07-19 16:59:41 +0300

    Exercise 12.9: Set up Redis for the project, complete

--- todo-app/todo-backend/redis/index.js
+++ todo-app/todo-backend/redis/index.js
@@ -2,6 +2,7 @@ const redis = require('redis')
 const { promisify } = require('util')
 const { REDIS_URL } = require('../util/config')
 
+
 let getAsync
 let setAsync
 
@@ -16,9 +17,9 @@ if (!REDIS_URL) {
   const client = redis.createClient({
     url: REDIS_URL
   })
-    
+
   getAsync = promisify(client.get).bind(client)
-  setAsync = promisify(client.set).bind(client)    
+  setAsync = promisify(client.set).bind(client)
 }
 
 module.exports = {
--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,5 +1,11 @@
+require('dotenv').config()
+
 const MONGO_URL = process.env.MONGO_URL || undefined
 const REDIS_URL = process.env.REDIS_URL || undefined
+
+console.log('mongo url', MONGO_URL)
+console.log('Redis url', REDIS_URL)
+
 
 module.exports = {
   MONGO_URL,//: 'mongodb://the_username:the_password@localhost:3456/the_database',
