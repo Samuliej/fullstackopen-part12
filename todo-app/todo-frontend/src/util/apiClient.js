9de7e0873966209da869ad4761c01ed6ca7dc38e
Samuli Toppi <samuli.toppi@outlook.com>
2024-07-22 14:28:36 +0300

    Exercise 12.13: Todo application frontend, complete

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const url = import.meta.env.VITE_BACKEND_URL
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: url,
 })
 
 export default apiClient
